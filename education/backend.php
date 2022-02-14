e569b336b903df34793c2a0b13bb244ceda0d8b0
radhik504 <95910703+radhik504@users.noreply.github.com>
2022-02-14 12:58:59 +0530

    Update backend.php

--- education/backend.php
+++ education/backend.php
@@ -1,8 +1,8 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
-$database="contact";
+$servername="sql207.epizy.com";
+$username="epiz_31057389";
+$password="aaE5CU4JWu";
+$database="epiz_31057389_contact";
 
 //create connection
 $conn=mysqli_connect($servername,$username,$password,$database);
